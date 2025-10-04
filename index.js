bafa9554b337fb9ec40d918428aa551d4f4ed42b
kuxala <cyberdna12@gmail.com>
2025-10-04 12:43:04 +0400

    Add Express to satisfy Vercel requirements

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // This file exists only to satisfy Vercel's build process
 // The actual serverless function is in api/webhook.js
-module.exports = {};
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Use /webhook endpoint for ClickUp webhooks' });
+});
+
+module.exports = app;
